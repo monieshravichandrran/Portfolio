e82e96c56c2f298068b8ad61542a372a19992ad7
monieshravichandrran <monieshravichandraan@gmail.com>
2022-01-01 07:42:47 -0800

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "./App.css";
+import Zoom from "react-reveal/Zoom";
+import Font, { Text } from "react-font";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="flex justify-center mt-10">
+        <Zoom duration={3000} top>
+          <Font family="Monoton">
+            <h1 className="text-white text-3xl">MONIESH &nbsp; RAVICHANDRAN</h1>
+          </Font>
+        </Zoom>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
